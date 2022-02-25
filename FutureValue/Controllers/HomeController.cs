a39b201166b8267316204f79c98e7bbabb96566f
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-25 11:20:39 -0500

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using FutureValue.Models;
+
+namespace FutureValue.Controllers {
+
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            ViewBag.Name = "Mary"; // ViewBag is a dictionary to store data and the data
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+
+        [HttpPost] // Correlates to the Index.cshtml with the post
+        public IActionResult Index(FutureValueModel model) {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FutureValue.Models {
+
+    public class FutureValueModel {
+
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue() {
+            var months = Years * 12; // Could do int months here but decides to use var
+            var monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            var futureValue = 0m; // The m forces it to be a decimal since without the m, var will default to an integer
+            for(var i = 0; i < months; i++) {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
